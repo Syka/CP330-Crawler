5317ffce5cbe2a1b32015b910240879678a1201c
Evan Soloway <esolowa1@confederationc.on.ca>
2014-11-13 18:12:29 -0500

    Updating

--- Project/Project/Weapons.cs
+++ Project/Project/Weapons.cs
@@ -1,77 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Project
-{
+//namespace Project
+//{
     
-    class SelectWeapon
-    {
-        int WeaponDamage = 0;
-    }
+//    class SelectWeapon
+//    {
+//        int WeaponDamage = 0;
+//    }
 
-    public interface WeaponBehaviour
-    {
-        void useWeapon();
-        int weaponDamage = 0;
+//    public interface WeaponBehaviour
+//    {
+//        void useWeapon();
+//        int weaponDamage = 0;
 
-    }
-    public class AxeBehaviour : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Swing with the Axe");
+//    }
+//    public class AxeBehaviour : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Swing with the Axe");
             
-        }
+//        }
 
         
-    }
-    public class SwordBehaviour : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Slice with the Sword");
-        }
-    }
+//    }
+//    public class SwordBehaviour : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Slice with the Sword");
+//        }
+//    }
 
-    public class LongSwordBehaviour : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Slice with the Long Sword");
-        }
-    }
+//    public class LongSwordBehaviour : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Slice with the Long Sword");
+//        }
+//    }
 
-    public class BowAndArrow : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You fire an Arrow");
-        }
-    }
-    public class KnifeBehaviour : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Stab with the Knife");
-        }
-    }
-    public class Mace : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Swing the Mace");
-        }
-    }
-    public class BattleHammer : WeaponBehaviour
-    {
-        public void useWeapon()
-        {
-            Console.WriteLine("You Smash the BattleHammer");
-        }
-    }
+//    public class BowAndArrow : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You fire an Arrow");
+//        }
+//    }
+//    public class KnifeBehaviour : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Stab with the Knife");
+//        }
+//    }
+//    public class Mace : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Swing the Mace");
+//        }
+//    }
+//    public class BattleHammer : WeaponBehaviour
+//    {
+//        public void useWeapon()
+//        {
+//            Console.WriteLine("You Smash the BattleHammer");
+//        }
+//    }
 
     
-}
+//}
