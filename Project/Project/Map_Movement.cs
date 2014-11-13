db324509f29708e3ff30d078dc159d01e40962bb
David Modin <dmodin1@confederationc.on.ca>
2014-11-13 10:20:37 -0500

    Made the Map playable from Main (for testing purposes)

--- Project/Project/Map_Movement.cs
+++ Project/Project/Map_Movement.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace gameWorld
+namespace Project
 {
     public class Map
     {
--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -10,6 +10,13 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Map map = new Map(10);
+            while (!map.dead)
+            {
+                Console.Clear();
+                Console.WriteLine("GAME OVER");
+                Console.ReadLine();
+            }
         }
     }
 }
