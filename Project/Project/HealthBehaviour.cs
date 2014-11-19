8ac8cfd90ae0bf7d33a3f2b644d3fab28a2d36d9
Nolan Wood <nwood@confederationc.on.ca>
2014-11-19 10:33:28 -0500

    Removed using from Health interface

---
+++ Project/Project/HealthBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Project
+{
+    //Base Health values for Heroes and Bosses
+    public interface IHealthBehaviour
+    {
+        void Health();
+    }
+
+    public class HighHealth : IHealthBehaviour
+    {
+        public void Health()
+        {
+            //Health = 20
+        }
+    }
+
+    public class MedHealth : IHealthBehaviour
+    {
+        public void Health()
+        { 
+            //Health = 15
+        }
+    }
+
+    public class LowHealth : IHealthBehaviour
+    { 
+        public void Health()
+        {
+            //Health 10
+        }
+    }
+
+    public class HeroHealthHigh : IHealthBehaviour
+    {
+        public void Health()
+        { 
+            //Health 25
+        }
+    }
+
+    public class HeroHealthLow : IHealthBehaviour
+    {
+        public void Health()
+        {
+            //Health 15
+        }
+
+    }
+}
