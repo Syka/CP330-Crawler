8de56455e111fc54fddf804e4f21c1b8b108c9ab
Nolan Wood <nwood@confederationc.on.ca>
2014-11-26 10:28:40 -0500

    uncommented jordans work

--- Project/Project/Weapons.cs
+++ Project/Project/Weapons.cs
@@ -6,51 +6,51 @@ using System.Threading.Tasks;
 
 namespace Project
 {
-    
-    //public interface WeaponBehaviour
-    //{
-        
-    //}
 
-    //public class Sword : WeaponBehaviour
-    //{
-    //    int durability = 8;
-    //    int damage = 5;
+    public interface WeaponBehaviour
+    {
 
-    //    public void useWeapon()
-    //    {
-            
-            
+    }
 
-    //    }
-        
+    public class Sword : WeaponBehaviour
+    {
+        int durability = 8;
+        int damage = 5;
 
-    //}
-
-    //public class Sword : WeaponBehaviour
-    //{
-    //    int durability = 8;
-    //    int damage = 5;
-
-    //    public void useWeapon()
-    //    {
-    //        Console.WriteLine("Sword Durability: {0}", durability);
-    //        if (durability > 0)
-    //        {
-    //            Console.WriteLine("You slash with the sword!");
-    //            durability = durability - 2;
-    //            damage = 5;
-    //        }
-
-    //        else
-    //        {
-    //            Console.WriteLine("Your Sword broke!!!");
-    //        }
+        public void useWeapon()
+        {
 
 
-    //    }
+
+        }
 
 
-    //}
+    }
+
+    public class Sword : WeaponBehaviour
+    {
+        int durability = 8;
+        int damage = 5;
+
+        public void useWeapon()
+        {
+            Console.WriteLine("Sword Durability: {0}", durability);
+            if (durability > 0)
+            {
+                Console.WriteLine("You slash with the sword!");
+                durability = durability - 2;
+                damage = 5;
+            }
+
+            else
+            {
+                Console.WriteLine("Your Sword broke!!!");
+            }
+
+
+        }
+
+
+    }
     
 }
