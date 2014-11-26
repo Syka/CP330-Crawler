4481543406e64d99d921f047604edb88735cc39c
Jordan <jwolkows@confederationc.on.ca>
2014-11-26 10:09:08 -0500

    Commited

---
+++ Project/Project/Weapons.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    
+    //public interface WeaponBehaviour
+    //{
+        
+    //}
+
+    //public class Sword : WeaponBehaviour
+    //{
+    //    int durability = 8;
+    //    int damage = 5;
+
+    //    public void useWeapon()
+    //    {
+            
+            
+
+    //    }
+        
+
+    //}
+
+    //public class Sword : WeaponBehaviour
+    //{
+    //    int durability = 8;
+    //    int damage = 5;
+
+    //    public void useWeapon()
+    //    {
+    //        Console.WriteLine("Sword Durability: {0}", durability);
+    //        if (durability > 0)
+    //        {
+    //            Console.WriteLine("You slash with the sword!");
+    //            durability = durability - 2;
+    //            damage = 5;
+    //        }
+
+    //        else
+    //        {
+    //            Console.WriteLine("Your Sword broke!!!");
+    //        }
+
+
+    //    }
+
+
+    //}
+    
+}
