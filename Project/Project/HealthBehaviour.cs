199e99ee55488ece6dc81da9e8f1d9e1ef81d729
Nolan Wood <nwood@confederationc.on.ca>
2014-12-02 09:56:21 -0500

    Entered in random health values to be added to base health

--- Project/Project/HealthBehaviour.cs
+++ Project/Project/HealthBehaviour.cs
@@ -9,54 +9,57 @@ namespace Project
     public interface IHealthBehaviour
     {
         void Health();
+        
     }
 
     public class HighHealth : IHealthBehaviour
     {
         public int valueHealth;
+        Random randomHealth = new Random();
         public void Health()
         {
-            valueHealth = 20;
+            
+            valueHealth = 20 + randomHealth.Next(3,10);
         }
     }
 
     public class MedHealth : IHealthBehaviour
     {
         public int valueHealth;
-
+        Random randomHealth = new Random();
         public void Health()
         {
-            valueHealth = 10;
+            valueHealth = 10 + randomHealth.Next(1,7);
         }   
     }
 
     public class LowHealth : IHealthBehaviour
     {
         public int valueHealth;
-
+        Random randomHealth = new Random();
         public void Health()
         {
-            valueHealth = 10;
+            valueHealth = 10 + randomHealth.Next(0,5);
         }
     }
 
     public class HeroHealthHigh : IHealthBehaviour
     {
         public int valueHealth;
-
+        Random randomHealth = new Random();
         public void Health()
         { 
-            valueHealth = 25;
+            valueHealth = 25 + randomHealth.Next(5,20);
         }
     }
 
     public class HeroHealthLow : IHealthBehaviour
     {
         public int valueHealth;
-
+        Random randomHealth = new Random();
         public void Health()
         {
-            valueHealth = 15;
+            valueHealth = 15 + randomHealth.Next(0,10);
         }
 
     }
